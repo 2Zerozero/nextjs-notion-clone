4e2b3d14591daba95e8ee65d0cbeb916d512d023
2Zerozero <hawgae@gmail.com>
2024-05-27 13:48:32 +0900

    ✨ Feat: heading - [landing]

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./_components/Heading";
+
+const LandingPage = () => {
+  return (
+    <div className="flex min-h-full flex-col">
+      <div className="flex flex-1 flex-col items-center justify-center gap-y-8 px-6 pb-10 text-center md:justify-start">
+        <Heading />
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
