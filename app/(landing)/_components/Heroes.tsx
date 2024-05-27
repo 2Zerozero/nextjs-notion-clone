2f5cbaf651f6c666aed7a0c726358cea1844bbc6
2Zerozero <hawgae@gmail.com>
2024-05-27 17:28:03 +0900

    ✨ Feat: heroes - [landing]

---
+++ app/(landing)/_components/Heroes.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import React from "react";
+
+const Heroes = () => {
+  return (
+    <div className="flex max-w-5xl flex-col items-center justify-center">
+      <div className="flex items-center">
+        <div className="relative h-[300px] w-[300px] sm:h-[300px] sm:w-[300px] md:h-[300px] md:w-[300px]">
+          <Image
+            src="/documents.png"
+            fill
+            className="object-contain"
+            alt="Documents"
+          />
+        </div>
+        <div className="relative hidden h-[400px] w-[400px] md:block">
+          <Image
+            src="/reading.png"
+            fill
+            className="object-contain"
+            alt="Reading"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Heroes;
