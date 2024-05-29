2e57201db1b5181b5ea76f704cc97497d79b8ae7
2Zerozero <hawgae@gmail.com>
2024-05-30 00:31:34 +0900

    ✨ Feat: darkmode - [landing]

--- app/(landing)/_components/Logo.tsx
+++ app/(landing)/_components/Logo.tsx
@@ -12,7 +12,20 @@ import React from "react";
 const Logo = () => {
   return (
     <div className="hidden items-center gap-x-2 md:flex">
-      <Image src="/logo.svg" width="40" height="40" alt="Logo" />
+      <Image
+        src="/logo.svg"
+        width="40"
+        height="40"
+        alt="Logo"
+        className="dark:hidden"
+      />
+      <Image
+        src="/logo-dark.svg"
+        width="40"
+        height="40"
+        alt="Logo"
+        className="hidden dark:block"
+      />
       <p className={cn("font-semibold", font.className)}>Notion</p>
     </div>
   );
--- app/(landing)/_components/Navbar.tsx
+++ app/(landing)/_components/Navbar.tsx
@@ -3,19 +3,20 @@ import { useScrollTrack } from "@/hooks/use-scroll-track";
 import { cn } from "@/lib/utils";
 import React from "react";
 import Logo from "./Logo";
+import { ModeToggle } from "@/components/mode-toggle";
 
 const Navbar = () => {
   const scrolled = useScrollTrack();
   return (
     <div
       className={cn(
-        "fixed top-0 z-50 flex w-full items-center bg-background p-6",
+        "fixed top-0 z-50 flex w-full items-center bg-background p-6 dark:bg-[#1f1f1f]",
         scrolled && "border-b shadow-sm",
       )}
     >
       <Logo />
       <div className="flex w-full items-center justify-between gap-x-2 md:ml-auto md:justify-end">
-        Login
+        <ModeToggle />
       </div>
     </div>
   );
