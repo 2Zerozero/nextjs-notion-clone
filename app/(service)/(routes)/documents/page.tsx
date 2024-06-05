f5b9db1b927b9a261f34d235eac57fa05f36e33f
YoungWoo Lee <hawgae@gmail.com>
2024-06-05 13:31:13 +0900

    ✨ Feat: Notion Document APIs 생성 - Document 생성

--- app/(service)/(routes)/documents/page.tsx
+++ app/(service)/(routes)/documents/page.tsx
@@ -5,8 +5,24 @@ import { useUser } from "@clerk/clerk-react";
 import { PlusCircle } from "lucide-react";
 import Image from "next/image";
 
+import { api } from "@/convex/_generated/api";
+import { useMutation } from "convex/react";
+import { toast } from "sonner";
+
 const DocumentsPage = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({ title: "test" });
+
+    toast.promise(promise, {
+      loading: "Create a new document...",
+      success: "Mew Document Created!",
+      error: "Failed to create a new document...",
+    });
+  };
+
   return (
     <div className="flex h-full flex-col items-center justify-center space-y-4">
       <Image
@@ -26,7 +42,7 @@ const DocumentsPage = () => {
       <h2 className="text-lg font-medium">
         Welcom to {user?.firstName}&apos;s Notion
       </h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className="mr-2 h-4 w-4" />
         Create a document
       </Button>
