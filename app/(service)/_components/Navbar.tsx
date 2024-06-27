fa317d0677b2aa148bc11dd652a84467e58f7a39
2Zerozero <hawgae@gmail.com>
2024-06-27 22:19:06 +0900

    ✨ Feat: Notion Document Banner Part1 - [배너]

--- app/(service)/_components/Navbar.tsx
+++ app/(service)/_components/Navbar.tsx
@@ -6,6 +6,7 @@ import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import { Title } from "./Title";
+import { Banner } from "./Banner";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -44,6 +45,7 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
           <Title initialData={document} />
         </div>
       </nav>
+      {document.isArchived && <Banner documentId={document._id} />}
     </>
   );
 };
