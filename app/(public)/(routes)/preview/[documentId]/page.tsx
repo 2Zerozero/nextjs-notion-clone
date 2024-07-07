9d3386e67a29eba2440b67d3eef616cfe6b4155e
2Zerozero <hawgae@gmail.com>
2024-07-07 14:17:41 +0900

    ✨ Feat: Notion Document Preview 마무리

---
+++ app/(public)/(routes)/preview/[documentId]/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { useMutation, useQuery } from "convex/react";
+import { Id } from "@/convex/_generated/dataModel";
+import Toolbar from "@/components/Toolbar";
+import { Cover } from "@/components/cover";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Editor } from "@/components/editor";
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
+
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  const update = useMutation(api.documents.update);
+
+  const onChange = (content: string) => {
+    update({
+      id: params.documentId,
+      content,
+    });
+  };
+
+  if (document === undefined) {
+    return (
+      <div>
+        <Cover.Skeleton />
+        <div className="mx-auto mt-10 md:max-w-3xl lg:max-w-4xl">
+          <div className="space-y-4 pl-8 pt-4">
+            <Skeleton className="h-14 w-[50%]" />
+            <Skeleton className="h-4 w-[80%]" />
+            <Skeleton className="h-4 w-[40%]" />
+            <Skeleton className="h-4 w-[60%]" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  if (document === null) {
+    return <div>Document Not Found...</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      <Cover preview url={document.coverImage} />
+      <div className="mx-auto md:max-w-3xl lg:max-w-4xl">
+        <Toolbar initialData={document} />
+        <Editor
+          onChange={onChange}
+          initialContent={document.content}
+          editable={false}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DocumentIdPage;
